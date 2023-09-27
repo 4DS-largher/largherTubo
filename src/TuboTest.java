a260f2d87b00d70732030555083c21d8a29cf257
pecoranera00 <pecoranera1929@gmail.com>
2023-09-28 00:00:02 +0200

    test: test method calculateOuterSurfaceArea

--- src/TuboTest.java
+++ src/TuboTest.java
@@ -3,5 +3,7 @@ public class TuboTest {
         Tubo t1 = new Tubo(20,22,10);
 
         System.out.println("The volume of the tube is: " + t1.calculateVolume());
+
+        System.out.println("The external surface area is: " + t1.calculateOuterSurfaceArea());
     }
 }
