11f0a255c54d48a85213d0210ed01f3c9271909e
pecoranera00 <pecoranera1929@gmail.com>
2023-09-27 23:57:33 +0200

    test: test method calculateVolume

--- src/TuboTest.java
+++ src/TuboTest.java
@@ -1,5 +1,7 @@
 public class TuboTest {
     public static void main(String[] args) {
         Tubo t1 = new Tubo(20,22,10);
+
+        System.out.println("The volume of the tube is: " + t1.calculateVolume());
     }
 }
